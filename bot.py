bfa276e30f772aa1dbb204228c64cb7bb7bac2af
Hiro Ogawa <pooh_mtb@hotmail.com>
2017-01-21 22:25:12 +0900

    　

--- bot.py
+++ bot.py
@@ -48,23 +48,69 @@ def print_profile(user_id):
         print_error(e)
 
 
+
+@handler.add(JoinEvent)
+def handle_join_message(event):
+    print(event)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=u'空前絶後のぉ〜〜〜〜〜〜'))
+    if(event.source.type == 'group'):
+        # group_id_temp = event.source.group_id
+        line_bot_api.push_message(
+            event.source.group_id,
+            TextSendMessage(text=u'グループIDは' + event.source.group_id + u'です'))
+        pass
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     print(event)
     if(event.source.type == 'user'):
         print_profile(event.source.user_id)
-        print(u'user')
-        print(event.source.user_id)
-        print(event.message.text)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text))
+
+        if(event.message.text == u'支払入力'):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=u'金額を入力してください'))
+            pass
+        elif(event.message.text == u'状況'):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=(
+                    u'Aさんが15400円使用しました\n'
+                    u'Bさんが2000円使用しました\n'
+                    u'Cさんが1200円使用しました\n'
+                    u'Dさんが0円使用しました\n'
+                    u'一人あたり4650円です'
+                    )))
+            pass
+        elif(event.message.text == u'ヘルプ'):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=u'ヘルプはまだない'))
+            pass
+        elif(event.message.text == u'精算'):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=u'一人あたり4650円です'))
+            pass
+        else:
+            print(u'user')
+            print(event.source.user_id)
+            print(event.message.text)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text))
+            line_bot_api.push_message(
+                'Cd374b84d45da9579f2f0cc7556d35255',
+                TextSendMessage(text=event.message.text))
         pass
 
     if(event.source.type == 'group'):
         print(u'group')
         print(event.source.group_id)
         print(event.message.text)
+        # group_id_temp = event.source.group_id
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text))
@@ -79,26 +125,22 @@ def save_content(message_id, filename):
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
     print(event)
-    save_content(event.message.id, 'static/' + event.message.id + '.jpg')
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=u'画像ありがと'))
-
-@handler.add(MessageEvent, message=VideoMessage)
-def handle_video_message(event):
-    print(event)
-    save_content(event.message.id, 'static/' + event.message.id + '.mp4')
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=u'動画ありがと'))
-
-@handler.add(MessageEvent, message=AudioMessage)
-def handle_audio_message(event):
-    print(event)
-    save_content(event.message.id, 'static/' + event.message.id + '.m4a')
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=u'音声ありがと'))
+    if(event.source.type == 'user'):
+        save_content(event.message.id, 'static/' + event.message.id + '.jpg')
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=u'画像ありがと'))
+        # print('groupid:' + group_id_temp)
+        line_bot_api.push_message(
+            '',
+            TextSendMessage(text=u'新しい画像がアップロードされたよ'))
+        line_bot_api.push_message(
+            '',
+            ImageSendMessage(
+                original_content_url=base_url + '/static/' + event.message.id + '.jpg',
+                preview_image_url=base_url + '/static/' + event.message.id + '.jpg'
+            )
+        )
 
 @handler.add(PostbackEvent)
 def handle_postback_message(event):
